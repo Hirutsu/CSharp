21de50b4205aa954707845d9c1e14fdb8bc81c10
Hirutsu <official_hirutsu@mail.com>
2021-02-17 18:53:04 +0400

    Practice 1 and 2

---
+++ PracticeCSharp/PracticeCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PracticeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words;
+            using (StreamReader streamReader = new StreamReader(@"C:\Users\offic\source\repos\PracticeCSharp\PracticeCSharp\TextFile1.txt", false))
+            {
+                string allText = streamReader.ReadToEnd();
+                words = allText.Split();
+            }
+            BinaryTree binaryTree = new BinaryTree();
+            for(int index = 0; index < words.Length;index++)
+            {
+                binaryTree.Add(Convert.ToInt32(words[index]));
+            }
+            //Подсчет суммы через прямой проход
+            Console.WriteLine(binaryTree.SumOddNums());
+            Console.WriteLine(binaryTree.SumEvenNums());
+            Console.WriteLine("Last element(самый глубокий):" + binaryTree.SearchDepthNode(250));
+            Console.WriteLine("Глубина корня:"+ binaryTree.SearchDepthNode(5));
+            Console.WriteLine("Глубина потомка корня:" + binaryTree.SearchDepthNode(-2));
+        }
+    }
+}
---
+++ PracticeCSharp/PracticeCSharp/class BinaryTree.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp
+{
+    public class BinaryTree //класс, реализующий АТД «дерево бинарного поиска»
+    {
+        //вложенный класс, отвечающий за узлы и операции допустимы для дерева бинарного поиска
+        private class Node
+        {
+            public object inf; //информационное поле
+            public Node left; //ссылка на левое поддерево
+            public Node rigth; //ссылка на правое поддерево
+            public Node(object nodeInf) //конструктор вложенного класса, создает узел дерева
+            {
+                inf = nodeInf;
+                left = null;
+                rigth = null;
+            }
+            //добавляет узел в дерево так, чтобы дерево оставалось деревом бинарного поиска
+            public static void Add(ref Node r, object nodeInf)
+            {
+                if (r == null)
+                {
+                    r = new Node(nodeInf);
+                }
+                else
+                {
+                if (((IComparable)(r.inf)).CompareTo(nodeInf) > 0)
+                    {
+                        Add(ref r.left, nodeInf);
+                    }
+                    else
+                    {
+                        Add(ref r.rigth, nodeInf);
+                    }
+                }
+            }
+            public static int SumOddNums(Node r) //прямой обход дерева
+            {
+                if (r != null)
+                {
+                    if (Convert.ToInt32(r.inf) % 2 != 0)
+                    {
+                        return Convert.ToInt32(r.inf) + SumOddNums(r.left) + SumOddNums(r.rigth);
+                    }
+                    else
+                    {
+                        return SumOddNums(r.left) + SumOddNums(r.rigth);
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            public static int SumEvenNums(Node r) //прямой обход дерева
+            {
+                if (r != null)
+                {
+                    if (Convert.ToInt32(r.inf) % 2 == 0)
+                    {
+                        return Convert.ToInt32(r.inf) + SumEvenNums(r.left) + SumEvenNums(r.rigth);
+                    }
+                    else
+                    {
+                        return SumEvenNums(r.left) + SumEvenNums(r.rigth);
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            public static void Preorder(Node r) //прямой обход дерева
+            {
+                if (r != null)
+                {
+                    Console.Write("{0} ", r.inf);
+                    Preorder(r.left);
+                    Preorder(r.rigth);
+                }
+            }
+            public static void Inorder(Node r) //симметричный обход дерева
+            {
+                if (r != null)
+                {
+                    Inorder(r.left);
+                    Console.Write("{0} ", r.inf);
+                    Inorder(r.rigth);
+                }
+            }
+            public static void Postorder(Node r) //обратный обход дерева
+            {
+                if (r != null)
+                {
+                    Postorder(r.left);
+                    Postorder(r.rigth);
+                    Console.Write("{0} ", r.inf);
+                }
+            }
+            //поиск ключевого узла в дереве
+            public static void Search(Node r, object key, out Node item)
+            {
+                if (r == null)
+                {
+                    item = null;
+                }
+                else
+                {
+                    if (((IComparable)(r.inf)).CompareTo(key) == 0)
+                    {
+                        item = r;
+                    }
+                    else
+                    {
+                        if (((IComparable)(r.inf)).CompareTo(key) > 0)
+                        {
+                            Search(r.left, key, out item);
+                        }
+                        else
+                        {
+                            Search(r.rigth, key, out item);
+                        }
+                    }
+                }
+            }
+
+            public static int SearchDepthNode(Node r, object key)
+            {
+                if (r == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (((IComparable)(r.inf)).CompareTo(key) == 0)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        if (((IComparable)(r.inf)).CompareTo(key) > 0)
+                        {
+                            return 1+SearchDepthNode(r.left, key);
+                        }
+                        else
+                        {
+                            return 1+SearchDepthNode(r.rigth, key);
+                        }
+                    }
+                }
+            }
+
+            //методы Del и Delete позволяют удалить узел в дереве так, чтобы дерево при этомоставалось деревом бинарного поиска
+            private static void Del(Node t, ref Node tr)
+            {
+                if (tr.rigth != null)
+                {
+                    Del(t, ref tr.rigth);
+                }
+                else
+                {
+                    t.inf = tr.inf;
+                    tr = tr.left;
+                }
+            }
+            public static void Delete(ref Node t, object key)
+            {
+                if (t == null)
+                {
+                    throw new Exception("Данное значение в дереве отсутствует");
+                }
+                else
+                {
+                    if (((IComparable)(t.inf)).CompareTo(key) > 0)
+                    {
+                        Delete(ref t.left, key);
+                    }
+                    else
+                    {
+                        if (((IComparable)(t.inf)).CompareTo(key) < 0)
+                        {
+                            Delete(ref t.rigth, key);
+                        }
+                        else
+                        {
+                            if (t.left == null)
+                            {
+                                t = t.rigth;
+                            }
+                            else
+                            {
+                            if (t.rigth == null)
+                                {
+                                    t = t.left;
+                                }
+                                else
+                                {
+                                    Del(t, ref t.left);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } //конец вложенного класса
+        Node tree; //ссылка на корень дерева
+                   //свойство позволяет получить доступ к значению информационного поля корня дерева
+        public object Inf
+        {
+            set { tree.inf = value; }
+            get { return tree.inf; }
+        }
+        public BinaryTree() //открытый конструктор
+        {
+            tree = null;
+        }
+        private BinaryTree(Node r) //закрытый конструктор
+        {
+            tree = r;
+        }
+        public void Add(object nodeInf) //добавление узла в дерево
+        {
+            Node.Add(ref tree, nodeInf);
+        }
+        //организация различных способов обхода дерева
+        public int SumOddNums()
+        {
+            return Node.SumOddNums(tree);
+        }
+        public int SumEvenNums()
+        {
+            return Node.SumEvenNums(tree);
+        }
+        public void Preorder()
+        {
+            Node.Preorder(tree);
+        }
+        public void Inorder()
+        {
+            Node.Inorder(tree);
+        }
+        public void Postorder()
+        {
+            Node.Postorder(tree);
+        }
+        //поиск ключевого узла в дереве
+        public BinaryTree Search(object key)
+        {
+        Node r;
+            Node.Search(tree, key, out r);
+            BinaryTree t = new BinaryTree(r);
+            return t;
+        }
+
+        public int SearchDepthNode(object key)
+        {
+            Node r;
+            return Node.SearchDepthNode(tree, key);
+        }
+
+
+        //удаление ключевого узла в дереве
+        public void Delete(object key)
+        {
+            Node.Delete(ref tree, key);
+        }
+    }
+}
