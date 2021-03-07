95a4850e0dafe16ac1bfac4c15a94187de593a4c
Hirutsu <official_hirutsu@mail.com>
2021-03-07 12:01:38 +0400

    New projects

---
+++ HuffmanAlgorithm/HuffmanAlgorithm/Haffman.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanAlgorithm
+{
+        public class Haffman
+        {
+            private class Node
+            {
+                public object inf;
+                public int number;
+                public int weight;
+                public Node left;
+                public Node rigth;
+
+                public Node(object nodeInf) //конструктор вложенного класса, создает узел дерева
+                {
+                    inf = nodeInf;
+                    number = 0;
+                    weight = 0;
+                    left = null;
+                    rigth = null;
+                }
+
+                public static void Add(ref Node r, int weight)
+                {
+                    Dictionary<Node,int> list = new Dictionary<Node, int>();
+                    
+                }    
+            }
+            Node tree;
+
+            public Haffman()//открытый конструктор
+            {
+                tree = null;
+            }
+            private Haffman(Node r)
+            {
+                tree = r;
+            }
+            public void Add(Dictionary<char, int> tempDic)
+            {
+                Node.Add();
+            }
+    }
+}
--- HuffmanAlgorithm/HuffmanAlgorithm/Program.cs
+++ HuffmanAlgorithm/HuffmanAlgorithm/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace HuffmanAlgorithm
 {
@@ -10,6 +9,25 @@ namespace HuffmanAlgorithm
     {
         static void Main(string[] args)
         {
+            string str = Console.ReadLine();//аук32432ква324 и23
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            for(int index =0;index<str.Length;index++)
+            {
+                if(dic.ContainsKey(str[index]))
+                {
+                    dic[str[index]]++;
+                }
+                else
+                {
+                    dic.Add(str[index], 1);
+                }
+            }
+            foreach(var item in dic)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+            Haffman tree = new Haffman();
+            tree.Add(dic);
         }
     }
 }
