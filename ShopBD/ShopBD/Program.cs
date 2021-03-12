7000a87d24cdd9d270cb2b615ce579c4aff2f2a1
Hirutsu <official_hirutsu@mail.com>
2021-03-12 10:49:42 +0400

    Complete ShopBD

--- ShopBD/ShopBD/Program.cs
+++ ShopBD/ShopBD/Program.cs
@@ -10,6 +10,81 @@ namespace ShopBD
     {
         static void Main(string[] args)
         {
+            LinkedList<Shop> listShop = new LinkedList<Shop>();
+            Console.WriteLine("Write size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for (int index = 0; index < size; index++)
+            {
+                Console.WriteLine("Write name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Write price");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Write Sale");
+                int sale = Convert.ToInt32(Console.ReadLine());
+
+                Nullable<DateTime> dateTimeStart;
+                Nullable<DateTime> dateTimeEnd;
+                if (sale==0)
+                {
+                    dateTimeStart = null;
+                    dateTimeEnd = null;
+                }
+                else
+                {
+                    Console.WriteLine("Write DateTime Sale Start");
+                    dateTimeStart = Convert.ToDateTime(Console.ReadLine());
+
+                    Console.WriteLine("Write DateTime Sale End");
+                    dateTimeEnd = Convert.ToDateTime(Console.ReadLine());
+                }
+
+                Shop shop = new Shop(name, price, sale, dateTimeStart, dateTimeEnd);
+                listShop.AddLast(shop);
+            }
+
+            Console.WriteLine("Полный список товаров");
+            foreach (var item in listShop)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Акционные товары");
+            foreach (var item in listShop)
+            {
+                if (item.GetSale() > 0)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            listShop.ElementAt(0).SetPrice(250);
+            Console.WriteLine("Изменный полный список товаров");
+            foreach (var item in listShop)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Поиск по названию,введите название");
+            string findName = Console.ReadLine();
+            foreach(var item in listShop)
+            {
+                if(item.GetName() == findName)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Console.WriteLine("Поиск товаров с акцией,введите дату");
+            DateTime data = Convert.ToDateTime(Console.ReadLine());
+            foreach(var item in listShop)
+            {
+                if( (item.GetStartSale()<=data) &&(item.GetEndSale()>=data))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
         }
     }
 }
---
+++ ShopBD/ShopBD/Shop.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBD
+{
+    class Shop
+    {
+        static int _incID = 0;
+        private int _id;
+        private string _nameProduct;
+        private double _priceProduct;
+        private int _sale;
+        private DateTime? _dateTimeSaleStart;
+        private DateTime? _dateTimeSaleEnd;
+
+        public Shop(string nameProduct, double priceProduct, int sale, DateTime? dtStart, DateTime? dtEnd)
+        {
+            _id = _incID;
+            _nameProduct = nameProduct;
+            _priceProduct = priceProduct;
+            _sale = sale;
+            _incID++;
+            _dateTimeSaleStart = dtStart;
+            _dateTimeSaleEnd = dtEnd;
+        }
+        
+        public override string ToString()
+        {
+            return "[id=" + _id + 
+                   "; name=" + _nameProduct + 
+                   "; price=" + _priceProduct + 
+                   "; sale=" + _sale + 
+                   "; discount price=" + (_priceProduct - _priceProduct * (_sale / 100.0))+
+                   "; dtStart="+_dateTimeSaleStart+"; dtEnd="+_dateTimeSaleEnd+"]";
+        }
+        public void SetNameProduct(string nameProduct)
+        {
+            this._nameProduct = nameProduct;
+        }
+        public void SetPrice(double priceProduct)
+        {
+            this._priceProduct = priceProduct;
+        }
+        public void SetSale(int sale)
+        {
+            this._sale = sale;
+        }
+        public void SetAll(string nameProduct, double priceProduct, int sale)
+        {
+            this._nameProduct = nameProduct;
+            this._priceProduct = priceProduct;
+            this._sale = sale;
+        }
+        public int GetSale()
+        {
+            return this._sale;
+        }
+        public string GetName()
+        {
+            return this._nameProduct;
+        }
+        public DateTime? GetStartSale()
+        {
+            return this._dateTimeSaleStart;
+        }
+        public DateTime? GetEndSale()
+        {
+            return this._dateTimeSaleEnd;
+        }
+    }
+}
